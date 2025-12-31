7c56bd28464be4ae856d25a5d27ee558a34e1f69
AlexSaSp <sancoyosp@gmail.com>
2025-12-31 11:36:18 +0100

    Deletes unnecessary comments

--- 2025/Day11/day11.py
+++ 2025/Day11/day11.py
@@ -10,6 +10,7 @@ def find_paths(current: str, target: str, neighbours: dict[str, list[str]]) -> i
         sum_paths += find_paths(neighbour, target, neighbours)
     return sum_paths
 
+
 def find_paths_dac_fft(current: str, target: str, neighbours: dict[str, list[str]], dac: bool, fft: bool, cache: dict[tuple[str, bool, bool], int]) -> int:
     if current == target:
         return 1 if dac and fft else 0
@@ -45,18 +46,7 @@ def main():
         neighbours[devices[0]] = devices[1].split( )
     
     print("Paths you -> out", find_paths("you", "out", neighbours))
-    # cache = {}
     print("Paths svr -> out via dac and fft", find_paths_dac_fft("svr", "out", neighbours, False, False, {}))
-    # print(cache)
-    # print("Paths svr -> out via dac and fft",
-    #       find_paths("svr", "dac", neighbours)
-    #       * find_paths("dac", "fft", neighbours)
-    #       * find_paths("fft", "out", neighbours)
-    #       + find_paths("svr", "fft", neighbours)
-    #       * find_paths("fft", "dac", neighbours)
-    #       * find_paths("dac", "out", neighbours))
-
-
 
 
 if __name__ == "__main__":
